4978a5f53fa2f5dfc5935cc54af2f531467c6fd8
ezekiel <favourtobiloba200@gmail.com>
2026-06-12 14:46:59 +0100

    fix the database ssue

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
 
-  synchronize: process.env.NODE_ENV !== "production",
+  synchronize: process.env.TYPEORM_SYNC === "true",
 
   entities: [Tweet],
   logging: true,
